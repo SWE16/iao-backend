56629649fd7e1501b385187704ecd1180585341c
regexbuster <benjamin@gofranks.com>
2024-07-04 15:01:23 -0400

    refactor for future express api use

---
+++ CURRENT/api.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.sendFile({
+        message: 'Hello World from Express API backend!',
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
